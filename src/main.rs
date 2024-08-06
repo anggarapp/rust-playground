0c6f0d65e3c1a5b20fa4a50af4e2368b714ba099
Anggara Permana Putra <anggaraperpe@gmail.com>
2024-08-06 12:04:46 +0700

    try [crates]: dotenv

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,11 @@ fn main() {
 }
 
 #[test]
-fn test_using_env() {}
+fn test_using_env() {
+    use dotenv;
+
+    assert_eq!(
+        dotenv::var("TEST_ENV").unwrap(),
+        "maidenlesstarnished".to_string()
+    );
+}
